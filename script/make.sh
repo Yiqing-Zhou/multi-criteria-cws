09ee79afb08049115dfc2e09ec562cc9c7715f01
yiqing <yiqzhou@microsoft.com>
2018-08-23 11:42:08 +0000

    pytorch

--- script/make.sh
+++ script/make.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-python3 ./make_dataset.py --training-data  data/$1/bmes/train-all.txt --dev-data data/$1/bmes/dev.txt \
---test-data data/$1/bmes/test.txt -o dataset/$1/dataset.pkl
+python ./make_dataset.py --training-data  final/all_train.txt --dev-data final/all_dev.txt \
+--test-data final/all_test.txt -o dataset/final_all/dataset.pkl
+
+#python ./make_dataset.py --training-data  data/$1/bmes/train-all.txt --dev-data data/$1/bmes/dev.txt \
+#--test-data data/$1/bmes/test.txt -o dataset/$1/dataset.pkl
--- script/train.sh
+++ script/train.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-python3 model.py --dataset dataset/$1/dataset.pkl --num-epochs 60 \
---word-embeddings data/embedding/character.vec \
---log-dir result/$1 \
---dropout 0.2 \
+python main.py --dataset dataset/$1/dataset.pkl --num-epochs 10 \
+--char-embeddings data/embedding/character.vec \
+--output-dir output/$1 \
+--dropout 0.5 \
 --learning-rate 0.01 \
 --learning-rate-decay 0.9 \
 --hidden-dim 100 \
---dynet-seed $RANDOM \
 --bigram \
---skip-dev \
 ${@:2}
